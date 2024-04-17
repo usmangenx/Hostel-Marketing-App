b05d61ea2b2d8e55fb2de39c260ae5b26b9919eb
Muhammad Usman Javed <musmanjaved5552@gmail.com>
2024-04-18 03:37:33 +0500

    footer color update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex justify-center mt-6 flex-col items-center bg-slate-400 text-white max-w-[1240px] mx-auto h-[4rem]">
+    <div className="flex justify-center mt-6 flex-col items-center bg-[#5f5f5f] text-white max-w-[1240px] mx-auto h-[4rem]">
       <h1 className="text-md md:text-md font-poppins font-normal">
         © 2024 UmeAbeehaHostel. All rights reserved.
       </h1>
